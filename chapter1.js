dfd118c7b33c5462c02586fa3d3fccf88c723459
Sami Peralahti <sami.peralahti@gmail.com>
2017-01-03 09:50:11 +0200

    First commit, started learing JS

---
+++ chapter1.js
@@ -0,0 +1,25 @@
+/*
+a = 21;
+b = a * 2;
+console.log( b );
+*/
+
+// alert( b ); to popup window
+
+/* Brings up a prompt
+
+age = prompt( "Please tell me your age:");
+console.log( age );
+*/
+
+/*
+
+You need var in every progra, asi it's the primary way you delcare (aka create) vairables.
+
+var a = 20;
+
+a = a +1;
+a = a *2;
+
+console.log( a );
+*/
